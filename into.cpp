8ced06d7fb52e87228e4cc3c2aab468ecf197ad3
SamuelXVII <sasmamaw41@gmail.com>
2025-09-17 18:56:56 -0700

    Add files via upload

---
+++ into.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+
+
+int main(void) 
+{ 
+	cout << "\nHello Inventory System!"; 
+	cout << "\n\nBelow are the int float and char outputs.\n"; 
+	cout << "\t" <<sizeof(int) << " bytes are in an integer, and it displays whole numbers\n"; 
+	cout << "\t" <<sizeof(float) << " bytes are in a float and it displays decimal values.\n"; 
+	cout << "\t" <<sizeof(char) << " bytes are in a character and they hold a singular character\n"; 
+	return 0; 
+} 
